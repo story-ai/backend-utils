ace9b3a5f524e603224555917e1075082d1e1534
Christopher Little <hephistocles@gmail.com>
2018-04-25 12:23:51 +0100

    Add subheader to prismic navigation type

--- src/types/PrismicTypes.ts
+++ src/types/PrismicTypes.ts
@@ -1,114 +1,115 @@
-import {
-  RichTextElement,
-  LinkElement,
-  EmbedElement,
-  ImageElement,
-  SelectElement,
-  SliceElement,
-  PrismicDocumentData,
-  DocumentLinkElement
-} from "prismic-javascript";
+import { Types } from "prismic-javascript";
+import { LinkElement } from "../../../../prismic-javascript/d.ts/types";
 
 export namespace PrismicTypes {
   export type PageContent =
-  	| SliceElement<
-      	"text_section",
-      	{},
-      	{
-          title: RichTextElement;
-          filled: SelectElement<"Filled" | "Unfilled">;
-          content: RichTextElement;
-      	}
+    | Types.SliceElement<
+        "text_section",
+        {},
+        {
+          title: Types.RichTextElement;
+          filled: Types.SelectElement<"Filled" | "Unfilled">;
+          content: Types.RichTextElement;
+        }
       >
-  	| SliceElement<
-      	"feature_section",
-      	{
-          image: ImageElement;
-          content: RichTextElement;
+    | Types.SliceElement<
+        "feature_section",
+        {
+          image: Types.ImageElement;
+          content: Types.RichTextElement;
           cta_text: string;
-          cta_link: LinkElement;
-      	},
-      	{
-          title: RichTextElement;
-          filled: SelectElement<"Filled" | "Unfilled">;
-      	}
+          cta_link: Types.LinkElement;
+        },
+        {
+          title: Types.RichTextElement;
+          filled: Types.SelectElement<"Filled" | "Unfilled">;
+          cards: Types.SelectElement<"With Background" | "Without Background">;
+          columns: number;
+        }
       >
-  	| SliceElement<
-      	"image_section",
-      	{},
-      	{
-          title: RichTextElement;
-          filled: SelectElement<"Filled" | "Unfilled">;
-          content: RichTextElement;
-          placement: SelectElement<"Left" | "Right">;
-          image: ImageElement;
-      	}
+    | Types.SliceElement<
+        "image_section",
+        {},
+        {
+          title: Types.RichTextElement;
+          filled: Types.SelectElement<"Filled" | "Unfilled">;
+          content: Types.RichTextElement;
+          placement: Types.SelectElement<"Left" | "Right">;
+          image: Types.ImageElement;
+        }
       >
-  	| SliceElement<
-      	"video_section",
-      	{},
-      	{
-          title: RichTextElement;
-          filled: SelectElement<"Filled" | "Unfilled">;
-          content: RichTextElement;
-          placement: SelectElement<"Left" | "Right">;
-          link: EmbedElement;
-      	}
+    | Types.SliceElement<
+        "video_section",
+        {},
+        {
+          title: Types.RichTextElement;
+          filled: Types.SelectElement<"Filled" | "Unfilled">;
+          content: Types.RichTextElement;
+          placement: Types.SelectElement<"Left" | "Right">;
+          link: Types.EmbedElement;
+        }
       >;
 
-  export interface Course extends PrismicDocumentData<PageContent> {
-  	title: RichTextElement;
-  	price: number;
-  	century_course_id: string;
-  	description: RichTextElement;
-  	rating?: number;
-  	subject?: string;
-  	level?: string;
-  	strands: {
+  export interface Course extends Types.PrismicDocumentData<PageContent> {
+    title: Types.RichTextElement;
+    price: number;
+    century_course_id: string;
+    description: Types.RichTextElement;
+    rating?: number;
+    subject?: string;
+    level?: string;
+    strands: {
       strand_id: string;
       title: string;
       time: string;
       difficulty: string;
-      description: RichTextElement;
-  	}[];
+      description: Types.RichTextElement;
+    }[];
   }
 
-  export interface Page extends PrismicDocumentData<PageContent> {
-  	title: RichTextElement;
-  	hero_content: RichTextElement;
-  	hero_ctas: {
-      link: LinkElement;
+  export interface Page extends Types.PrismicDocumentData<PageContent> {
+    title: Types.RichTextElement;
+    hero_content: Types.RichTextElement;
+    hero_ctas: {
+      link: Types.LinkElement;
       text: string;
-      filled: SelectElement<"Yes" | "No">;
-  	}[];
-  	century: SelectElement<"Yes" | "No">;
-  	skiparrow: SelectElement<"Yes" | "No">;
+      filled: Types.SelectElement<"Yes" | "No">;
+    }[];
+    century: Types.SelectElement<"Yes" | "No">;
+    skiparrow: Types.SelectElement<"Yes" | "No">;
   }
 
   export interface NavigationItem
-  	extends PrismicDocumentData<
-      	| SliceElement<"navigation_item", {}, { text: string; link: LinkElement }>
-      	| SliceElement<"divider">
+    extends Types.PrismicDocumentData<
+        | Types.SliceElement<"subheader", {}, { text: string }>
+        | Types.SliceElement<
+            "navigation_item",
+            {},
+            { text: string; link: Types.LinkElement }
+          >
+        | Types.SliceElement<"divider">
       > {
-  	text: string;
-  	link: LinkElement;
+    text: string;
+    link: Types.LinkElement;
   }
 
-  export interface WebsiteSettings extends PrismicDocumentData {
-  	_title: RichTextElement;
-  	homepage: DocumentLinkElement;
-  	logo: ImageElement;
-  	navigation: {
-      item: DocumentLinkElement<{
-      	text: string;
-      }>;
-  	}[];
-  	copyright: string;
-  	footer_socials: {
-      icon: string;
+  export interface WebsiteSettings extends Types.PrismicDocumentData {
+    _title: Types.RichTextElement;
+    homepage: Types.DocumentLinkElement;
+    logo: Types.ImageElement;
+    footer_navigation: {
+      text: string;
       link: LinkElement;
-  	}[];
+    }[];
+    navigation: {
+      item: Types.DocumentLinkElement<{
+        text: string;
+      }>;
+    }[];
+    copyright: string;
+    footer_socials: {
+      icon: string;
+      link: Types.LinkElement;
+    }[];
   }
-
 }
-
